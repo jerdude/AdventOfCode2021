4aab613a79b00a4bfe56025de224e9602a4fa8cf
jerdude <jer157@gmail.com>
2021-12-02 16:18:23 -0500

    Shortening day 1

--- Challenges/Day1.cs
+++ Challenges/Day1.cs
@@ -7,15 +7,6 @@ namespace AdventOfCode2021
 {
     class Day1
     {
-        private int[] _inputs;
-
-        public Day1()
-        {
-            _inputs = File.ReadLines("Inputs\\Day1.txt")
-                        .Select(x => int.Parse(x))
-                        .ToArray();
-        }
-
         public (long solution, long bonus) GetSolution()
         {
             return (GetPart1(),GetPart2());
@@ -23,6 +14,10 @@ namespace AdventOfCode2021
 
         private int GetPart1()
         {
+            var _inputs = File.ReadLines("Inputs\\Day1.txt")
+                        .Select(x => int.Parse(x))
+                        .ToArray();
+
             int last = int.MaxValue;
             int total = 0;
             foreach(int i in _inputs)
@@ -36,8 +31,31 @@ namespace AdventOfCode2021
             return total;
         }
 
+        private int GetPart1Short()
+        {
+            var _inputs = File.ReadLines("Inputs\\Day1.txt")
+                        .Select(x => int.Parse(x))
+                        .ToArray();
+
+            int last = int.MaxValue;
+            int total = 0;
+            foreach(int i in _inputs)
+            {
+                if(i > last)
+                    total++;
+
+                last = i;
+            }
+
+            return _inputs.Skip(1).Where(i => _inputs[i] > _inputs[i-1]).Count();
+        }
+
         private int GetPart2()
         {
+            var _inputs = File.ReadLines("Inputs\\Day1.txt")
+                        .Select(x => int.Parse(x))
+                        .ToArray();
+
             int last = int.MaxValue;
             int total = 0;
 
